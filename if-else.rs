1af375cb7d60c8bed6c407881897b3450a3afb8e
Shishir Dey <shishirdey241097@gmail.com>
2018-07-14 20:54:40 +0530

    1.2

---
+++ if-else.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x = 3;
+    if x <= 0 {
+        println!("{} is negative", x);
+    }
+    else if x == 0 {
+        println!("x is equal to zero");
+    }
+    else {
+        println!("{} is positive", x);
+    }
+}
