5b5b7376249f9c7bfafa81ce6412ae2ba8516214
Shishir Dey <shishirdey241097@gmail.com>
2018-07-14 20:03:01 +0530

    1.0

---
+++ hello/target/debug/deps/hello-1df2f5792a445115.d
@@ -0,0 +1,5 @@
+/home/altarf02/Desktop/Rust-101/hello/target/debug/deps/hello-1df2f5792a445115: src/main.rs
+
+/home/altarf02/Desktop/Rust-101/hello/target/debug/deps/hello-1df2f5792a445115.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/altarf02/Desktop/Rust-101/hello/target/debug/hello: /home/altarf02/Desktop/Rust-101/hello/src/main.rs
