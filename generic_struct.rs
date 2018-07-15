d3bdabc532cdc4fdc8ee0b2c33d77640cedc246a
Shishir Dey <shishirdey241097@gmail.com>
2018-07-15 20:59:41 +0530

    1.9

---
+++ generic_struct.rs
@@ -0,0 +1,9 @@
+fn main() {
+    struct Pair<K, V> {
+        key: K,
+        value: V,
+    }
+
+    let data_1: Pair<u8, String> = Pair { key: 2, value: "Abc".to_string() };
+    println!("Data_1 Key: {} Value: {}", data_1.key, data_1.value);
+}
---
+++ string.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut s = String::from("This is a string");
+    let mut s2 = "This is another string".to_string();
+
+    println!("{}\n{}", s, s2);
+    
+    s.remove(0);
+    s2.push('.');
+
+    println!("{}\n{}", s, s2);
+}
+
+/* Refer to https://doc.rust-lang.org/std/string/struct.String.html */
