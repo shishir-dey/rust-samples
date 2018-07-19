bbcea7ce883511fb2cd0a1d1902661a7ba1e993d
Shishir Dey <shishirdey241097@gmail.com>
2018-07-19 17:39:16 +0530

    2.2

---
+++ src/hashmap.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut db = HashMap::new();
+    
+    db.insert("A", "1");
+    db.insert("B", "2");
+    db.insert("C", "3");
+
+    for (k, v) in &db {
+        println!("Key: {} Value: {}", k, v);
+    }
+}
