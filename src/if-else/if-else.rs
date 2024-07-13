fddeb68624f924913ced68d751a5bd2cc15dcbaa
Shishir Dey <shishirdey241097@gmail.com>
2024-07-13 16:28:04 +0530

    Delete redundant code

--- src/if-else/if-else.rs
+++ src/if-else/if-else.rs
@@ -8,20 +8,3 @@ fn main() {
         println!("{} is positive", x);
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_if_else() {
-        let x = 3;
-        if x <= 0 {
-            assert_eq!(format!("{} is negative", x), "3 is negative");
-        } else if x == 0 {
-            assert_eq!(format!("x is equal to zero"), "x is equal to zero");
-        } else {
-            assert_eq!(format!("{} is positive", x), "3 is positive");
-        }
-    }
-}
